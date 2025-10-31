9fc28a4b874d39484fcb7c9f2a84403ddbfecdc5
prashanthglen <prashanthglen@gmail.com>
2025-10-31 22:38:02 +0100

    code cleanup from helloplugin

--- justifystatusbar.koplugin/main.lua
+++ justifystatusbar.koplugin/main.lua
@@ -1,53 +1,49 @@
 --[[--
-This is a debug plugin to test Plugin functionality.
+This is a plugin to justify all entries in the status bar so that they
+are equally spaced apart.
 
-@module koplugin.HelloWorld
---]]--
+=======
+@module koplugin.JustifyStatusBar
+--]]
+--
 
 -- This is a debug plugin, remove the following if block to enable it
 if true then
-    return { disabled = true, }
+	return { disabled = true }
 end
 
-local Dispatcher = require("dispatcher")  -- luacheck:ignore
+local Dispatcher = require("dispatcher") -- luacheck:ignore
 local InfoMessage = require("ui/widget/infomessage")
-local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
 
-local Hello = WidgetContainer:extend{
-    name = "hello",
-    is_doc_only = false,
-}
+local JustifySB = WidgetContainer:extend({
+	name = "justifyStatusBar",
+	is_doc_only = false,
+})
 
-function Hello:onDispatcherRegisterActions()
-    Dispatcher:registerAction("helloworld_action", {category="none", event="HelloWorld", title=_("Hello World"), general=true,})
+function JustifySB:onDispatcherRegisterActions()
+	Dispatcher:registerAction(
+		"justify_sb_action",
+		{ category = "none", event = "justify", title = _("Justify Status Bar"), general = true }
+	)
 end
 
-function Hello:init()
-    self:onDispatcherRegisterActions()
-    self.ui.menu:registerToMainMenu(self)
+function JustifySB:init()
+	self:onDispatcherRegisterActions()
+	self.ui.menu:registerToMainMenu(self)
 end
 
-function Hello:addToMainMenu(menu_items)
-    menu_items.hello_world = {
-        text = _("Hello World"),
-        -- in which menu this should be appended
-        sorting_hint = "more_tools",
-        -- a callback when tapping
-        callback = function()
-            UIManager:show(InfoMessage:new{
-                text = _("Hello, plugin world"),
-            })
-        end,
-    }
+function JustifySB:addToMainMenu(menu_items)
+	menu_items.hello_world = {
+		text = _("Justify Items"),
+		-- in which menu this should be appended
+		sorting_hint = "status_bar",
+	}
 end
 
-function Hello:onHelloWorld()
-    local popup = InfoMessage:new{
-        text = _("Hello World"),
-    }
-    UIManager:show(popup)
+function JustifySB:onHelloWorld()
+	-- perform justification over status bar here
 end
 
-return Hello
+return JustifySB
