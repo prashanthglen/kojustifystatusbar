18089b906ab4bd7da70fbe894b68538f1baa6156
prashanthglen <prashanthglen@gmail.com>
2025-11-06 18:23:10 +0100

    move plugin to a separate branch

--- justifystatusbar.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-	name = "justify_statusbar",
-	fullname = _("Justify Statusbar"),
-	description = _([[This plugin allows for equally spacing apart all entries in the status bar.]]),
-}
